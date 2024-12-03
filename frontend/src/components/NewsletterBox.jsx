31a0956f5c2ede1b786d19a34634958c3e8bcbb5
SeneshAnujaya <senesh9637@gmail.com>
2024-12-03 22:29:46 +0530

    create login, signup & about pages

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -10,8 +10,8 @@ const NewsletterBox = () => {
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
         <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto'>
-            <input type='email' placeholder='Enter your email' className='w-full sm:flex-1 outline-none' required/>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-1 mx-auto mt-6'>
+            <input type='email' placeholder='Enter your email' className='w-full sm:flex-1 outline-none border px-4 py-3' required/>
             <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
         </form>
     </div>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import Title from '../components/Title';
+import {assets} from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt='about-img'/>
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque iusto ipsa deleniti rerum tenetur facilis placeat minima libero! Repellendus dolorem reprehenderit doloribus ab impedit adipisci debitis ipsum excepturi vitae beatae!</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae consequatur adipisci fugit consectetur! Asperiores, inventore, quis aut explicabo rem facilis voluptatum laudantium porro veniam fugiat beatae exercitationem veritatis amet ex!</p>
+          <b className='text-gray-800'>Our Misson</b>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, nihil.</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quam accusantium placeat sapiente tenetur quisquam!</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Convenient:</b>
+        <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quam accusantium placeat sapiente tenetur quisquam!</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Exceptional Customer Service:</b>
+        <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quam accusantium placeat sapiente tenetur quisquam!</p>
+        </div>
+         </div>
+
+      <NewsletterBox />
+    </div>
   )
 }
 
