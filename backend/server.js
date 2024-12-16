6120731288420a4a91d60738601ae49f6492ca53
SeneshAnujaya <senesh9637@gmail.com>
2024-12-17 02:42:54 +0530

    mono repo commit second

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDb from './config/mongodb.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000
+connectDb()
+connectCloudinary()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+
+// Api Endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+
+app.get('/', (req, res) => {
+res.send("API WORKING");
+})
+
+app.listen(port, () => {
+    console.log("Server listen on PORT :" + port);
+    
+})
